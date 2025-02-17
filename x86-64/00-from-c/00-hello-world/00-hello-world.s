2f5c4e345b9501977fdd24609f11631f5a49a894
David Ordovskiy <david.ordovsky@gmail.com>
2025-02-17 18:56:52 -0500

    start x64, add hello-world.c and the version compiled into assembly

---
+++ x86-64/00-from-c/00-hello-world/00-hello-world.s
@@ -0,0 +1,21 @@
+	.file	"00-hello-world.c"
+	.intel_syntax noprefix
+	.text
+	.section	.rodata
+.LC0:
+	.string	"Hello, world!"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+	push	rbp
+	mov	rbp, rsp
+	lea	rax, .LC0[rip]
+	mov	rdi, rax
+	call	puts@PLT
+	mov	eax, 0
+	pop	rbp
+	ret
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 13.3.0-6ubuntu2~24.04) 13.3.0"
+	.section	.note.GNU-stack,"",@progbits
